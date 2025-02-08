99d11cbc10711d578073a4440715f968aed65cdc
sameh-123 <ssammehh582001.kkillo582001@gmail.com>
2025-02-08 02:26:55 +0200

    added the dark mode toggle to sign in page

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -10,27 +10,30 @@ export default async function Home() {
   const session = await auth();
   if (!session) {
     return (
-      <div className="container mx-auto h-screen flex items-center justify-center">
-        <div className="rounded-lg shadow-md dark:shadow-gray-400 p-10 w-96 flex flex-col items-center gap-5">
-          <div className="font-bold text-lg">Welcome to</div>
-          <Image src={logo} alt="instagram" className="dark:invert w-full" />
-          <p className="font-extralight text-2xl text-center">
-            enjoy sharing moments with others
-          </p>
-          <form
-            action={async () => {
-              'use server';
-              await signIn('google');
-            }}
-          >
-            <button
-              type="submit"
-              className="bg-igOrange hover:bg-igRed font-semibold text-white px-4 py-2 rounded-2xl flex items-center gap-1"
+      <div className=" h-screen flex flex-col w-full">
+        <SettingsBar />
+        <div className="container mx-auto w-full flex items-center justify-center mt-10">
+          <div className="rounded-lg shadow-md dark:shadow-gray-400 p-10 w-96 flex flex-col items-center gap-5">
+            <div className="font-bold text-lg">Welcome to</div>
+            <Image src={logo} alt="instagram" className="dark:invert w-full" />
+            <p className="font-extralight text-2xl text-center">
+              enjoy sharing moments with others
+            </p>
+            <form
+              action={async () => {
+                'use server';
+                await signIn('google');
+              }}
             >
-              sign in with google
-              <Image src={google} alt="google" className="size-8" />
-            </button>
-          </form>
+              <button
+                type="submit"
+                className="bg-igOrange hover:bg-igRed font-semibold text-white px-4 py-2 rounded-2xl flex items-center gap-1"
+              >
+                sign in with google
+                <Image src={google} alt="google" className="size-8" />
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     );
@@ -62,7 +65,7 @@ export default async function Home() {
   });
   return (
     <div className="container mx-auto ">
-      <SettingsBar/>
+      <SettingsBar />
       <StatusBar profiles={profiles} />
       <HomePosts posts={posts} Email={myEmail} />
     </div>
